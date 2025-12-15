96fa7ef753bc6357c422ee7d24eb516029a684e2
ChandanKumarWeb <chandan42kumar55@gmail.com>
2025-12-15 12:05:44 +0530

    code refactor

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -22,7 +22,7 @@ export default function Sidebar({
 
   return (
     <aside
-      className={`bg-gray-100 dark:bg-black border-r  border-black dark:border-white h-screen p-2 md:p-4 m-2 mr-0 ß transition-all duration-900
+      className={`bg-gray-100 dark:bg-black border-r  border-black dark:border-white h-screen p-2 md:p-4 m-2 mr-0 transition-all duration-900
         ${collapsed ? "hidden md:w-25 md:block" : "w-full md:w-58"}
       `}
     >
